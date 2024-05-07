9924b4839cf134ef5ce5d242fe7168930cd7e551
Rasmus E <fatman00hot@hotmail.com>
2024-05-07 10:11:26 +0200

    Added PYATS BFD configurator

--- src/pages/05_Pyats_BFD.py
+++ src/pages/05_Pyats_BFD.py
@@ -105,22 +105,7 @@ def nbwrapper_device(dev_name):
         return None
     
 @st.cache_data
-def nbwrapper_interface_all(dev_id):
-    # Connect to netbox
-    nb = pynetbox.api(st.session_state["netbox_url"], token=st.session_state["netbox_token"])  # Read only token
-    nb.http_session.verify = False
-
-    # Collecting all site switches of type cisco
-    allIf = nb.dcim.interfaces.filter(device_id=dev_id)
-    #st.write(list(allDevices))
-    allIf = list(allIf)
-    if allIf:
-        return allIf
-    else:
-        return None
-
-@st.cache_data
-def atswrapper_cdp(dev_ip, cli_username, cli_password):
+def atswrapper_bfd(dev_ip, cli_username, cli_password):
     atsnbrs = None
     testbed = loader.load(
         {
@@ -142,53 +127,94 @@ def atswrapper_cdp(dev_ip, cli_username, cli_password):
     atsdevice = testbed.devices[device["name"]]
     atsdevice.connect(via='cli', log_stdout=True, learn_hostname=True, connection_timeout=10)
 
-    atsnbrs = atsdevice.parse('show cdp neighbors detail')
+    atsnbrs = atsdevice.parse('show bfd neighbors details')
+    atsdevice.disconnect()
     return atsnbrs
+
+@st.cache_data
+def atswrapper_hsrp(dev_ip, cli_username, cli_password):
+    atsnbrs = None
+    testbed = loader.load(
+        {
+            "devices": {
+                device["name"]: {
+                    "connections": {"cli": {"protocol": "ssh", "ip": dev_ip, "init_exec_commands": [], "init_config_commands": []}}, # Find a better way to do this: https://github.com/CiscoTestAutomation/genielibs/issues/12
+                    "credentials": {
+                        "default": {
+                            "username": cli_username,
+                            "password": cli_password,
+                        }
+                    },
+                    "type": "Device",
+                    "os": "iosxe"
+                }
+            }
+        }
+    )
+    atsdevice = testbed.devices[device["name"]]
+    atsdevice.connect(via='cli', log_stdout=True, learn_hostname=True, connection_timeout=10)
+
+    atsnbrs = atsdevice.parse('show standby all')
+    atsdevice.disconnect()
+    return atsnbrs
+
+@st.cache_data
+def atswrapper_int(dev_ip, cli_username, cli_password):
+    atsnbrs = None
+    testbed = loader.load(
+        {
+            "devices": {
+                device["name"]: {
+                    "connections": {"cli": {"protocol": "ssh", "ip": dev_ip, "init_exec_commands": [], "init_config_commands": []}}, # Find a better way to do this: https://github.com/CiscoTestAutomation/genielibs/issues/12
+                    "credentials": {
+                        "default": {
+                            "username": cli_username,
+                            "password": cli_password,
+                        }
+                    },
+                    "type": "Device",
+                    "os": "iosxe"
+                }
+            }
+        }
+    )
+    atsdevice = testbed.devices[device["name"]]
+    atsdevice.connect(via='cli', log_stdout=True, learn_hostname=True, connection_timeout=10)
+
+    atsnbrs = atsdevice.parse('show interfaces')
+    atsdevice.disconnect()
+    return atsnbrs
+
+def atswrapper_config(dev_ip, cli_username, cli_password, configs):
+    atsnbrs = None
+    testbed = loader.load(
+        {
+            "devices": {
+                device["name"]: {
+                    "connections": {"cli": {"protocol": "ssh", "ip": dev_ip, "init_exec_commands": [], "init_config_commands": []}}, # Find a better way to do this: https://github.com/CiscoTestAutomation/genielibs/issues/12
+                    "credentials": {
+                        "default": {
+                            "username": cli_username,
+                            "password": cli_password,
+                        }
+                    },
+                    "type": "Device",
+                    "os": "iosxe"
+                }
+            }
+        }
+    )
+    atsdevice = testbed.devices[device["name"]]
+    atsdevice.connect(via='cli', log_stdout=True, learn_hostname=True, connection_timeout=10)
+
+    atsdevice.configure(configs)
+    atsdevice.disconnect()
 
 def selected_site_format(option):
     return f'{option["name"]}({option["short"]})'
 
 def selected_devices_format(option):
     return option["name"]
-
-def nb_if_patch(intid, parameter, value):
-    nb = pynetbox.api(st.session_state["netbox_url"], token=st.session_state["netbox_token"])  # Read only token
-    nb.http_session.verify = False
-    nbif_def = nb.dcim.interfaces.get(intid)
-    match parameter:
-        case "lag":
-            nbif_def.lag = value
-        case "name":
-            nbif_def.name = value
-    st.sidebar.write(f"{intid}: {parameter} = {value}")
-    nbif_def.save()
-
-def nb_cable_add(cable):
-    nb = pynetbox.api(st.session_state["netbox_url"], token=st.session_state["netbox_token"])  # Read only token
-    nb.http_session.verify = False
-
-    return nb.dcim.cables.create(cable)
-
-def nb_if_add(deviceid, ifdata):
-    nb = pynetbox.api(st.session_state["netbox_url"], token=st.session_state["netbox_token"])  # Read only token
-    nb.http_session.verify = False
-
-    ifdata['device']= deviceid
-    nb.dcim.interfaces.create(ifdata)
-
-def nb_dev_patch(deviceid, parameter, value):
-    nb = pynetbox.api(st.session_state["netbox_url"], token=st.session_state["netbox_token"])  # Read only token
-    nb.http_session.verify = False
-
-    nbdevice = nb.dcim.devices.get(deviceid)
-    match parameter:
-        case "serial":
-            nbdevice.serial = value
-        case "name":
-            nbdevice.name = value
-    nbdevice.save()
-    #st.write("Now we save")
-    #nbwrapper_devices.clear()
 
 
 st.title("Authentication")
@@ -239,68 +265,58 @@ if "netbox_url" in st.session_state and "netbox_token" in st.session_state:
     with st.expander("Show data"):
         st.write(selected_devices)
     if st.sidebar.toggle("Connection Allowed", False):
-        if st.button("Reload NB Data"):
-            nbwrapper_interface_all.clear()
-            nbwrapper_ip_address.clear()
+        if st.button("Reload ATS Data"):
+            atswrapper_bfd.clear()
+            atswrapper_int.clear()
+            atswrapper_hsrp.clear()
         for device in selected_devices:
             atsinterfaces = None
             try:
-                #atsinterfaces = atswrapper_interfaces(device["ip"], cli_username, cli_password)
-                atscdp = atswrapper_cdp(device["ip"], cli_username, cli_password)
-
+                atsint = atswrapper_int(device["ip"], cli_username, cli_password)
+                atshsrp = atswrapper_hsrp(device["ip"], cli_username, cli_password)
+                atsbfd = atswrapper_bfd(device["ip"], cli_username, cli_password)
 
             except Exception as error:
-                with st.container(border=True):
-                    st.subheader(f'{device["name"]}')
-                    st.error(error)
-                continue
+                if error != "Parser Output is empty":
+                    atsbfd = dict([])
+                    with st.container(border=True):
+                        st.subheader(f'{device["name"]}')
+                        st.error(error)
+                        print(error)
             
             with st.container(border=True):
                 hcol1, hcol2, hcol3 = st.columns([3,1,1])
                 hcol1.subheader(f'{device["name"]}')
                 hcol2.link_button("Netbox", f'https://netbox.dccat.dk/dcim/devices/{device["id"]}/', use_container_width=True)
-                fixall = hcol3.button("-> Fix all ->", key=f'{device["id"]}_sync_fixit', use_container_width=True, disabled=False)
-                allNbIf=nbwrapper_interface_all(device["id"])
-
-
-                neighbors = atscdp['index'] # Interfaces from PyATS
-                neighbors = neighbors.values()
-
-                #print(neighbors)
-                connections = []
-                for neighbor in neighbors:
-                    newConnection = {}
-                    newConnection['add_me'] = True
-                    newConnection['local_device'] = device["name"]
-                    newConnection['local_interface'] = neighbor['local_interface']
-                    newConnection['local_nbInterface'] = nbwrapper_interface(device["id"], neighbor['local_interface'])['id']
-                    newConnection['remote_device'] = neighbor['device_id'].split('.')[0]
-                    nbDevID = nbwrapper_device(neighbor['device_id'].split('.')[0])
-                    if nbDevID is not None:
-                        newConnection['remote_nbDevice'] = nbDevID['id']
-                        newConnection['remote_nbInterface'] = nbwrapper_interface(nbDevID['id'], neighbor['port_id'])['id']
-                    newConnection['remote_interface'] = neighbor['port_id']
-                    if nbwrapper_interface(device["id"], neighbor['local_interface']).get("cable") is not None:
-                        newConnection['add_me'] = False
-                    if len(newConnection.values()) == 8:
-                        connections.append(newConnection)
-                cableUpdate = st.data_editor(connections, use_container_width=True)
-                cableUpdate = [item for item in cableUpdate if item["add_me"]]
+                fixall = hcol3.button("-> Add BFD ->", key=f'{device["id"]}_sync_fixit', use_container_width=True, disabled=False)
                 
-                with st.expander(f'Raw Interface IP Data: {device["name"]}'):
-                    st.write(cableUpdate)
+                with st.expander(f'Raw BFD Data: {device["name"]}'):
+                    our_addr = atsbfd.get("our_address", [])
+                    allBFDNeiInt = []
+                    for addr in our_addr:
+                        neiaddr = our_addr[addr].get("neighbor_address", None)
+                        neiint = [nei.get("interface") for nei in neiaddr.values()]
+                        allBFDNeiInt.extend(neiint)
+                    st.write("BFD Interfaces")
+                    st.write(allBFDNeiInt)
+                    st.write("HSRP Interfaces")
+                    allHSRPInt = list(atshsrp.keys())
+                    st.write(allHSRPInt)
+                    missingBFDint = [int for int in allHSRPInt if int not in allBFDNeiInt]
+                    st.write(missingBFDint)
+
+
+                with st.expander(f'Raw interface Data: {device["name"]}', expanded=True):
+                    for interface in atsint:
+                        if interface in missingBFDint:
+                            st.warning(interface)
+
                 if fixall:
-                    for newCable in cableUpdate:
-                        cable = {
-                            "a_terminations": [{"object_type": "dcim.interface", "object_id": newCable['local_nbInterface']}],
-                            "b_terminations": [{"object_type": "dcim.interface", "object_id": newCable['remote_nbInterface']}]
-                        }
-                        
-                        try:
-                            print(nb_cable_add(cable))
-                        except Exception as e:
-                            print(str(e))
-                            #st.error(str(e))
+                    globalBFD = ['bfd-template single-hop DC-Template', 'interval min-tx 75 min-rx 75 multiplier 3']
+                    for int in missingBFDint:
+                        globalBFD.extend([f"interface {int}", "bfd template DC-Template"])
+                    st.write(atswrapper_config(device["ip"], cli_username, cli_password, globalBFD))
+                    #st.write(globalBFD)
 
             
             
